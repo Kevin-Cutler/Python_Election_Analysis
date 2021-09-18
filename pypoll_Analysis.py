c656dd731b75d972d64cf309addfc7b321174efb
kevin-cutler <kevinecutler86@gmail.com>
2021-09-17 22:32:43 -0400

    Updating Election Code for election_Analysiss repoUpdating Election Code for election_Analysiss repo

--- pypoll_Analysis.py
+++ pypoll_Analysis.py
@@ -57,7 +57,7 @@ with open(file_to_load) as election_data:
 # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
 
-# Print the final vote count to the terminal.
+     # Print the final vote count to the terminal.
     election_results = (
         f"\nElection Results\n"
         f"-------------------------\n"
@@ -66,15 +66,20 @@ with open(file_to_save, "w") as txt_file:
     print(election_results, end="")
     # Save the final vote count to the text file.
     txt_file.write(election_results)
-
-
+       
     for candidate_name in candidate_votes:
         # Retrieve vote count of a candidate.
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
         # Print each candidate, their voter count, and percentage to the
         # terminal.
-        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
 
         # Determine winning vote count, winning percentage, and candidate.  
         if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -90,4 +95,6 @@ with open(file_to_save, "w") as txt_file:
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
 
-    #print(winning_candidate_summary)
+    print(winning_candidate_summary)
+   # Save the winning candidate's results to the text file.
+    txt_file.write(winning_candidate_summary)
